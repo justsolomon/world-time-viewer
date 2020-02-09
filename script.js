2834e1e56a0ecae642f326f2cc500dc3b10e8296
Solomon <gbolahanbalogun5@gmail.com>
2020-02-09 22:11:26 +0100

    Add fetch request and async function to display and render area locations

---
+++ script.js
@@ -0,0 +1,29 @@
+const timezone = document.querySelector('#timezone-select');
+const areaLocation = document.querySelector('#location-select')
+
+function getAreaLocations() {
+	let area = timezone.value;
+	return fetch(`http://worldtimeapi.org/api/timezone/${area}`)
+			.then(res => res.json())
+			.catch(err => console.log(err));
+}
+
+function displayAreaLocations(locations) {
+	let area = timezone.value;
+	let markup = ``;
+	locations.forEach(location => {
+		markup += `
+			<option value=${location}>${location.slice(area.length + 1)}</option>
+		`
+	})
+	areaLocation.innerHTML = markup;
+}
+
+const renderAreaLocations = async function() {
+	const data = await getAreaLocations();
+	console.log(data);
+	displayAreaLocations(data);
+}
+
+timezone.addEventListener('change', renderAreaLocations)
+renderAreaLocations();
